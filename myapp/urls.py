a62bf3b06355e05c9400d4d40e4a4964682503f6
Nancy <nancygrace@gmail.com>
2025-03-04 16:25:02 +0100

    Initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.task_list, name="task_list"),
+    path("add/", views.create_task, name="create_task"),
+    path("edit/<int:pk>/", views.update_task, name="update_task"),
+    path("delete/<int:pk>/", views.delete_task, name="delete_task"),
+]
+
